c93959b10790feed2365429eb58c9c5472e3e201
dordsor21 <dordsor21@gmail.com>
2022-03-13 01:43:42 +0000

    Make sure thread-cached arrays are reset

--- spigot_v1_15_R2/src/main/java/com/sk89q/worldedit/bukkit/adapter/impl/fawe/BukkitAdapter_1_15_2.java
+++ spigot_v1_15_R2/src/main/java/com/sk89q/worldedit/bukkit/adapter/impl/fawe/BukkitAdapter_1_15_2.java
@@ -318,8 +318,12 @@ public final class BukkitAdapter_1_15_2 extends NMSAdapter {
             }
             return section;
         } catch (final Throwable e) {
-            Arrays.fill(blockToPalette, Integer.MAX_VALUE);
             throw e;
+        } finally {
+            Arrays.fill(blockToPalette, Integer.MAX_VALUE);
+            Arrays.fill(paletteToBlock, Integer.MAX_VALUE);
+            Arrays.fill(blockStates, 0);
+            Arrays.fill(blocksCopy, 0);
         }
     }
 
