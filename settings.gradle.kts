e79d7b5123a8c5c7e39fbbd8897ba67093862d38
Alexander Brandes <mc.cache@web.de>
2022-06-14 23:37:16 +0200

    chore: Rename output artifact

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,7 +17,7 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-rootProject.name = "worldedit-adapters"
+rootProject.name = "fastasyncworldedit-adapters"
 
 data class Ver(val minor: Int, val rel: String)
 
